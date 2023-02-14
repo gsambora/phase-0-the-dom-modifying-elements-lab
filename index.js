a44f768acea8da7245d80ae39d972537cd645dc5
Hana <grace.sambora@gmail.com>
2023-02-14 03:42:57 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElem = document.querySelector('main');
+const body = document.querySelector('body');
+body.removeChild(mainElem);
+
+body.innerHTML = "<h1>Meow!</h1>"
+const newHeader = body.querySelector('h1');
+
+newHeader.id = 'victory';
+newHeader.textContent= 'Hana is the champion';
